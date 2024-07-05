b16a4a62a0f28fd1babfeb20db868e7765c96218
Ada <obenedicta4.@gmail.com>
2024-07-05 08:30:57 +0100

    list comprehension

--- easy/watermelon.py
+++ easy/watermelon.py
@@ -1,7 +1,5 @@
 melon_kg = int(input().strip())
 
 # Weight in kilo must be greater than 2 and should be even
-if melon_kg > 2 and melon_kg % 2 == 0:
-    print("YES")
-else:
-    print("NO")
+result = ["YES" if melon_kg > 2 and melon_kg % 2 == 0 else "NO"]
+print(result)
