b9081b416464da55c4b78f2f0a0529d1b277ec6b
Ada <obenedicta4.@gmail.com>
2024-09-27 12:25:57 +0100

    update

--- easy/Python/Robin_Hood_Archery.py
+++ easy/Python/Robin_Hood_Archery.py
@@ -12,7 +12,6 @@ for num in range(archery):
     target_list = list(map(int, input().split(" "))) # Convert to integer
     if len(target_list) != target:
         print(f"Enter {target} numbers")
-        break
 
     # Loop through n number of queries checking if sheriff have a winning or tie chance
     for _ in range(query):
@@ -22,7 +21,6 @@ for num in range(archery):
         # Check if the query range is valid
         if l < 1 or r > len(target_list):
             print("Invalid query range")
-            break  # Skip this query if the range is invalid
 
         # Subset target_list based on query
         nums = sorted(target_list[l - 1:r], reverse=True)
