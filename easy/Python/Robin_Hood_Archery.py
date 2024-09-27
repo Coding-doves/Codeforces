56ecaf3845af79f2a4e10588ad41eeecfd52e322
Ada <obenedicta4.@gmail.com>
2024-09-27 12:15:27 +0100

    update

--- easy/Python/Robin_Hood_Archery.py
+++ easy/Python/Robin_Hood_Archery.py
@@ -1,21 +1,31 @@
 # Number of test cases to run
 archery = int(input())
 resp = []
+
 # Loop to receive n Number of test cases
 for num in range(archery):
     # Targets and number of times to query target list
     target, query = input().split(" ")
+    target, query = int(target), int(query)
     
     # Receive target list of test cases
-    target_list = input().split(" ")
-    if len(target_list) != int(target):
+    target_list = list(map(int, input().split(" "))) # Convert to integer
+    if len(target_list) != target:
         print(f"Enter {target} numbers")
+        continue
 
     # Loop through n number of queries checking if sheriff have a winning or tie chance
-    for _ in range(int(query)):
+    for _ in range(query):
         l, r = input().split(" ")
+        l, r = int(l), int(r)
+
+        # Check if the query range is valid
+        if l < 1 or r > len(target_list):
+            print("Invalid query range")
+            continue  # Skip this query if the range is invalid
+
         # Subset target_list based on query
-        nums = sorted(target_list[int(l) - 1:int(r)], reverse=True)
+        nums = sorted(target_list[l - 1:r], reverse=True)
 
         # Check Sheriff chances of winning/tie
         robin = 0
@@ -23,9 +33,9 @@ for num in range(archery):
         # Loop through test case/subset list
         for n in range(len(nums)):
             if n % 2 == 0:
-                robin += int(nums[n])
+                robin += nums[n]
             else:
-                sheriff += int(nums[n])
+                sheriff += nums[n]
 
         resp.append("NO" if robin > sheriff else "YES")
 
